b4ff0acdaa0004e2519540139fe7d4a8f1e9928f
kyfxbl <kyfxbl@gmail.com>
2014-06-10 15:23:11 +0800

    change name

---
+++ platform/ios/LosApp/LosAppDelegate.m
@@ -0,0 +1,27 @@
+#import "LosAppDelegate.h"
+
+@implementation LosAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    UIViewController *controller = [[FirstViewController alloc] initWithNibName:nil bundle:[NSBundle mainBundle]];
+    self.window.rootViewController = controller;
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+
+}
+
+@end
