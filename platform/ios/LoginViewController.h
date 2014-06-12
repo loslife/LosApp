7e12a117bd5d84b808a07272792d87869f2b332f
kyfxbl <kyfxbl@gmail.com>
2014-06-12 19:33:25 +0800

    https components

--- platform/ios/LoginViewController.h
+++ platform/ios/LoginViewController.h
@@ -1,6 +1,8 @@
 #import <UIKit/UIKit.h>
 #import "UserData.h"
 #import "BootstrapViewController.h"
+#import "LosHttpHelper.h"
+#import "LosAppUrls.h"
 
 @interface LoginViewController : UIViewController
 
