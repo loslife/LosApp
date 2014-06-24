b2019c191c0af8dc7d4a423fc869850a3eff35bb
kyfxbl <kyfxbl@gmail.com>
2014-06-24 11:34:44 +0800

    关联店铺的样式

--- platform/ios/AddShopView.m
+++ platform/ios/AddShopView.m
@@ -11,19 +11,14 @@
     self = [super init];
     if(self){
         
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, 320, 40)];
-        label.text = @"关联店铺";
-        label.textAlignment = NSTextAlignmentCenter;
-        label.textColor = [UIColor colorWithRed:18/255.0f green:172/255.0f blue:182/255.0f alpha:1.0f];
-        
-        self.phone = [[UITextField alloc] initWithFrame:CGRectMake(20, 160, 280, 40)];
+        self.phone = [[UITextField alloc] initWithFrame:CGRectMake(20, 80, 280, 40)];
         self.phone.placeholder = @"输入关联店铺的乐斯登陆账号";
         self.phone.borderStyle = UITextBorderStyleRoundedRect;
         self.phone.clearButtonMode = UITextFieldViewModeWhileEditing;
         [self.phone setKeyboardType:UIKeyboardTypeNumberPad];
         self.phone.delegate = self;
         
-        UIView *codeWrapper = [[UIView alloc] initWithFrame:CGRectMake(20, 220, 280, 40)];
+        UIView *codeWrapper = [[UIView alloc] initWithFrame:CGRectMake(20, 140, 280, 40)];
         
         self.code = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 180, 40)];
         self.code.placeholder = @"输入验证码";
@@ -45,7 +40,7 @@
         [codeWrapper addSubview:self.requireCodeButton];
         
         UIButton *attach = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-        attach.frame = CGRectMake(20, 280, 280, 40);
+        attach.frame = CGRectMake(20, 200, 280, 40);
         [attach setTitle:@"立即关联" forState:UIControlStateNormal];
         attach.backgroundColor = [UIColor colorWithRed:244/255.0f green:196/255.0f blue:82/255.0f alpha:1.0f];
         attach.tintColor = [UIColor whiteColor];
@@ -53,11 +48,12 @@
         [attach addTarget:controller action:@selector(appendEnterprise) forControlEvents:UIControlEventTouchUpInside];
         
         UITextView *notice = [[UITextView alloc] init];
-        notice.frame = CGRectMake(20, 320, 280, 120);
+        notice.frame = CGRectMake(20, 260, 280, 120);
         notice.text = @"小贴士：关联店铺完成后，在手机中可以查看店铺中的实时经营数据以及所有的会员资料。";
+        notice.font = [UIFont systemFontOfSize:16];
+        notice.textColor = [UIColor colorWithRed:103/255.0f green:103/255.0f blue:103/255.0f alpha:1.0f];
         notice.delegate = self;
         
-        [self addSubview:label];
         [self addSubview:self.phone];
         [self addSubview:codeWrapper];
         [self addSubview:attach];
