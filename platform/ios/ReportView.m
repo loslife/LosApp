8f1ae976f3fec1b9777f16917d685271a0c8a61b
kyfxbl <kyfxbl@gmail.com>
2014-06-30 17:19:46 +0800

    手势识别

--- platform/ios/ReportView.m
+++ platform/ios/ReportView.m
@@ -11,14 +11,35 @@
         LosTimePicker *dateSelectionBar = [[LosTimePicker alloc] initWithFrame:CGRectMake(0, 60, 320, 40) Delegate:controller InitDate:now type:DateDisplayTypeDay];
         
         UILabel *label = [[UILabel alloc] init];
-        label.frame = CGRectMake(20, 100, 280, 40);
+        label.frame = CGRectMake(0, 100, 320, 468);
         label.text = @"报表View";
         label.textAlignment = NSTextAlignmentCenter;
+        label.backgroundColor = [UIColor redColor];
+        
+        UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(pullRight)];
+        swipeLeft.direction = UISwipeGestureRecognizerDirectionLeft;
+        
+        UISwipeGestureRecognizer *swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(pullLeft)];
+        swipeRight.direction = UISwipeGestureRecognizerDirectionRight;
+        
+        [label addGestureRecognizer:swipeLeft];
+        [label addGestureRecognizer:swipeRight];
+        label.userInteractionEnabled = YES;
         
         [self addSubview:dateSelectionBar];
         [self addSubview:label];
     }
     return self;
+}
+
+-(void) pullRight
+{
+    NSLog(@"pull right");
+}
+
+-(void) pullLeft
+{
+    NSLog(@"pull left");
 }
 
 @end
