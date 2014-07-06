a2d75e99f3d772adb65fa40c601b7be292a52703
unknown <jtmyry@163.com>
2014-07-06 23:42:35 +0800

    base

---
+++ platform/android/src/com/yilos/losapp/common/ActivityControlUtil.java
@@ -0,0 +1,80 @@
+package com.yilos.losapp.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.util.Log;
+
+
+public class ActivityControlUtil {
+
+
+    /**
+     * activityList用以存储开启的Activity。
+     */
+    private static List<Activity> activityList = new ArrayList<Activity>();
+
+    public static List<Activity> getActivityList()
+    {
+        return activityList;
+    }
+
+    public static void setActivityList(List<Activity> activityList)
+    {
+        ActivityControlUtil.activityList = activityList;
+    }
+
+    /**
+     * 从列表中删除输入的Activity。<br>
+     * 
+     * @author 刘文杨
+     * @param 删除了的activity。
+     */
+    public static void remove(Activity activity)
+    {
+
+        activityList.remove(activity);
+
+    }
+
+    /**
+     * 在BaseActivity的onCreat中调用，每个继承了BaseActivity的Activity都会把自身传进activityList中。<br>
+     * 
+     * @author 刘文杨
+     * @param activity
+     */
+    public static void add(Activity activity)
+
+    {
+
+        activityList.add(activity);
+
+    }
+
+    /**
+     * 在list中取出activity逐个finish。<br>
+     * 
+     * @author 刘文杨
+     */
+    public static void finishAllActivities()
+    {
+        if (null == activityList.get(0))
+        {
+            Log.e("ActivityControlUtil",
+                    "ActivityControlUtil --- Well ,this path was blocked since no activity created.");
+        }
+        else
+        {
+            for (Activity activity : activityList)
+            {
+
+                activity.finish();
+
+            }
+        }
+
+    }
+
+
+}
