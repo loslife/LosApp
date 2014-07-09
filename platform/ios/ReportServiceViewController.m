e2b0eb983886215939ca6102387f1fa24d678cca
kyfxbl <kyfxbl@gmail.com>
2014-07-09 16:33:17 +0800

    datasource开发完成

--- platform/ios/ReportServiceViewController.m
+++ platform/ios/ReportServiceViewController.m
@@ -57,29 +57,52 @@
 
 -(NSString*) total
 {
-    return @"￥8888";
+    double sum = 0;
+    for(ServicePerformance *item in records){
+        sum += item.value;
+    }
+    return [NSString stringWithFormat:@"￥%f", sum];
 }
 
 -(NSUInteger) itemCount
 {
+    NSUInteger count = [records count];
+    if(count > 3){
+        return count - 3;
+    }
     return 0;
 }
 
 -(ServicePerformance*) itemAtIndex:(int)index
 {
-    return nil;
+    return [records objectAtIndex:index + 3];
 }
 
 #pragma mark - PieChart delegate
 
 -(NSUInteger) pieItemCount
 {
-    return 0;
+    if([records count] <= 4){
+        return [records count];
+    }
+    return 4;
 }
 
 -(LosPieChartItem*) pieItemAtIndex:(int)index
 {
-    return nil;
+    if(index < 3){
+        ServicePerformance *performance = [records objectAtIndex:index];
+        NSString *title = [NSString stringWithFormat:@"%d.%@%f", index + 1, performance.title, performance.ratio];
+        return [[LosPieChartItem alloc] initWithTitle:title Ratio:performance.ratio];
+    }
+    
+    double sum = 0.0;
+    
+    for(int i = 3; i < [records count]; i++){
+        ServicePerformance *performance = [records objectAtIndex:i];
+        sum += performance.ratio;
+    }
+    return [[LosPieChartItem alloc] initWithTitle:@"其他" Ratio:sum];
 }
 
 -(UIColor*) colorAtIndex:(int)index
