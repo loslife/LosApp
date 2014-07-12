796817d01c110b10c16e3a56affeabfd7088e2f0
kyfxbl <kyfxbl@gmail.com>
2014-07-12 20:14:26 +0800

    ip

--- platform/ios/LosAppUrls.h
+++ platform/ios/LosAppUrls.h
@@ -1,4 +1,4 @@
-#define ServerHost @"http://192.168.1.115:5000"
+#define ServerHost @"http://172.20.10.14:5000"
 
 #define LOGIN_URL [ServerHost stringByAppendingString:@"/svc/losapp/login"]
 #define REGISTER_URL [ServerHost stringByAppendingString:@"/svc/losapp/register"]
