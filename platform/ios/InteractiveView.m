25b1f3368cf4090441978ae8168ad07dcb1d038b
kyfxbl <kyfxbl@gmail.com>
2014-07-28 17:39:29 +0800

    操作引导页面

--- platform/ios/InteractiveView.m
+++ platform/ios/InteractiveView.m
@@ -14,36 +14,25 @@
         self.frame = CGRectMake(0, 0, 320, screenHeight);
         self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.6];
         
-        UIImageView *image1 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 70, 80)];
-        image1.center = CGPointMake(160, screenHeight / 2 - 80);
-        image1.image = [UIImage imageNamed:@"gesture_switch"];
+        UIImageView *image = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 70, 80)];
+        image.center = CGPointMake(160, screenHeight - 49);
+        image.image = [UIImage imageNamed:@"gesture_switch"];
         
-        UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
-        label1.center = CGPointMake(160, screenHeight / 2 - 20);
-        label1.text = @"1. 左右滑动，切换其他业绩";
-        label1.textAlignment = NSTextAlignmentCenter;
-        label1.textColor = [UIColor whiteColor];
-        
-        UIImageView *image2 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 60, 80)];
-        image2.center = CGPointMake(160, screenHeight / 2 + 60);
-        image2.image = [UIImage imageNamed:@"gesture_refresh"];
-        
-        UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
-        label2.center = CGPointMake(160, screenHeight / 2 + 120);
-        label2.text = @"2. 连续触摸屏幕2次，刷新业绩";
-        label2.textAlignment = NSTextAlignmentCenter;
-        label2.textColor = [UIColor whiteColor];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
+        label.center = CGPointMake(160, 210);
+        label.text = @"左右滑动，切换业绩";
+        label.textAlignment = NSTextAlignmentCenter;
+        label.textColor = [UIColor whiteColor];
         
         UIButton *iknow = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-        iknow.frame = CGRectMake(120, screenHeight - 89, 80, 40);
+        iknow.frame = CGRectMake(120, 240, 80, 40);
         [iknow setTitle:@"我知道了" forState:UIControlStateNormal];
         iknow.tintColor = BLUE1;
+        iknow.titleLabel.font = [UIFont systemFontOfSize:18];
         [iknow addTarget:delegate action:@selector(close) forControlEvents:UIControlEventTouchUpInside];
         
-        [self addSubview:image1];
-        [self addSubview:label1];
-        [self addSubview:image2];
-        [self addSubview:label2];
+        [self addSubview:image];
+        [self addSubview:label];
         [self addSubview:iknow];
     }
     return self;
